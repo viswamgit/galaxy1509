413e4f45626a57ef9828518d48e2a621f1c82ac6
galaxy1509 <viswam1107@gmail.com>
2022-09-15 17:38:08 +0530

    task

---
+++ src/org/test/Sample.java
@@ -0,0 +1,25 @@
+package org.test;
+
+public class Sample {
+	
+	
+	private void ryzen() {
+	System.out.println("tuff gaming");
+
+	}
+	
+	
+	private void gaming() {
+		System.out.println("gaming proceesor");
+
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.ryzen();
+		s.gaming();
+	}
+
+}
